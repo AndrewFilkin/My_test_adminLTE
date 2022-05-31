be66bc6d08df66444dc506950c388b27c5c3460b
AndrewFilkin <filkin.andrew.s@gmail.com>
2022-05-31 19:05:12 +0300

    admin panel work

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -180,7 +180,8 @@
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
 
         <a href="index3.html" class="brand-link">
-            <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+            <img src="{{asset('img/admin/AdminLTELogo.png')}}" alt="AdminLTE Logo"
+                 class="brand-image img-circle elevation-3"
                  style="opacity: .8">
             <span class="brand-text font-weight-light">AdminLTE 3</span>
         </a>
@@ -189,7 +190,7 @@
 
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                 <div class="image">
-                    <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                    <img src="{{asset('img/admin/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
                     <a href="#" class="d-block">Alexander Pierce</a>
@@ -237,7 +238,7 @@
                     </li>
                     <li class="nav-item">
                         <a href="#" class="nav-link">
-{{--                            <i class="nav-icon fas fa-th"></i>--}}
+                            {{--                            <i class="nav-icon fas fa-th"></i>--}}
                             <i class="nav-icon fa-brands fa-whatsapp"></i>
                             <p>
                                 Simple Link
@@ -254,82 +255,84 @@
 
     <div class="content-wrapper">
 
-        <div class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1 class="m-0">Starter Page</h1>
-                    </div>
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Starter Page</li>
-                        </ol>
-                    </div>
-                </div>
-            </div>
-        </div>
+        @yield('content')
+
+        {{--        <div class="content-header">--}}
+        {{--            <div class="container-fluid">--}}
+        {{--                <div class="row mb-2">--}}
+        {{--                    <div class="col-sm-6">--}}
+        {{--                        <h1 class="m-0">Starter Page</h1>--}}
+        {{--                    </div>--}}
+        {{--                    <div class="col-sm-6">--}}
+        {{--                        <ol class="breadcrumb float-sm-right">--}}
+        {{--                            <li class="breadcrumb-item"><a href="#">Home</a></li>--}}
+        {{--                            <li class="breadcrumb-item active">Starter Page</li>--}}
+        {{--                        </ol>--}}
+        {{--                    </div>--}}
+        {{--                </div>--}}
+        {{--            </div>--}}
+        {{--        </div>--}}
 
 
-        <div class="content">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">
-                                    Some quick example text to build on the card title and make up the bulk of the
-                                    card's
-                                    content.
-                                </p>
-                                <a href="#" class="card-link">Card link</a>
-                                <a href="#" class="card-link">Another link</a>
-                            </div>
-                        </div>
-                        <div class="card card-primary card-outline">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">
-                                    Some quick example text to build on the card title and make up the bulk of the
-                                    card's
-                                    content.
-                                </p>
-                                <a href="#" class="card-link">Card link</a>
-                                <a href="#" class="card-link">Another link</a>
-                            </div>
-                        </div>
-                    </div>
+        {{--        <div class="content">--}}
+        {{--            <div class="container-fluid">--}}
+        {{--                <div class="row">--}}
+        {{--                    <div class="col-lg-6">--}}
+        {{--                        <div class="card">--}}
+        {{--                            <div class="card-body">--}}
+        {{--                                <h5 class="card-title">Card title</h5>--}}
+        {{--                                <p class="card-text">--}}
+        {{--                                    Some quick example text to build on the card title and make up the bulk of the--}}
+        {{--                                    card's--}}
+        {{--                                    content.--}}
+        {{--                                </p>--}}
+        {{--                                <a href="#" class="card-link">Card link</a>--}}
+        {{--                                <a href="#" class="card-link">Another link</a>--}}
+        {{--                            </div>--}}
+        {{--                        </div>--}}
+        {{--                        <div class="card card-primary card-outline">--}}
+        {{--                            <div class="card-body">--}}
+        {{--                                <h5 class="card-title">Card title</h5>--}}
+        {{--                                <p class="card-text">--}}
+        {{--                                    Some quick example text to build on the card title and make up the bulk of the--}}
+        {{--                                    card's--}}
+        {{--                                    content.--}}
+        {{--                                </p>--}}
+        {{--                                <a href="#" class="card-link">Card link</a>--}}
+        {{--                                <a href="#" class="card-link">Another link</a>--}}
+        {{--                            </div>--}}
+        {{--                        </div>--}}
+        {{--                    </div>--}}
 
-                    <div class="col-lg-6">
-                        <div class="card">
-                            <div class="card-header">
-                                <h5 class="m-0">Featured</h5>
-                            </div>
-                            <div class="card-body">
-                                <h6 class="card-title">Special title treatment</h6>
-                                <p class="card-text">With supporting text below as a natural lead-in to additional
-                                    content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                        <div class="card card-primary card-outline">
-                            <div class="card-header">
-                                <h5 class="m-0">Featured</h5>
-                            </div>
-                            <div class="card-body">
-                                <h6 class="card-title">Special title treatment</h6>
-                                <p class="card-text">With supporting text below as a natural lead-in to additional
-                                    content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
+        {{--                    <div class="col-lg-6">--}}
+        {{--                        <div class="card">--}}
+        {{--                            <div class="card-header">--}}
+        {{--                                <h5 class="m-0">Featured</h5>--}}
+        {{--                            </div>--}}
+        {{--                            <div class="card-body">--}}
+        {{--                                <h6 class="card-title">Special title treatment</h6>--}}
+        {{--                                <p class="card-text">With supporting text below as a natural lead-in to additional--}}
+        {{--                                    content.</p>--}}
+        {{--                                <a href="#" class="btn btn-primary">Go somewhere</a>--}}
+        {{--                            </div>--}}
+        {{--                        </div>--}}
+        {{--                        <div class="card card-primary card-outline">--}}
+        {{--                            <div class="card-header">--}}
+        {{--                                <h5 class="m-0">Featured</h5>--}}
+        {{--                            </div>--}}
+        {{--                            <div class="card-body">--}}
+        {{--                                <h6 class="card-title">Special title treatment</h6>--}}
+        {{--                                <p class="card-text">With supporting text below as a natural lead-in to additional--}}
+        {{--                                    content.</p>--}}
+        {{--                                <a href="#" class="btn btn-primary">Go somewhere</a>--}}
+        {{--                            </div>--}}
+        {{--                        </div>--}}
+        {{--                    </div>--}}
 
-                </div>
+        {{--                </div>--}}
 
-            </div>
-        </div>
+        {{--            </div>--}}
+        {{--        </div>--}}
 
     </div>
 
